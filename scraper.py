5731e29e7ea4c0fe2a487d9e8bdf47f06920123a
Renisson Silva <renissonx@gmail.com>
2024-08-07 21:42:15 -0300

    Adicionando conexão com banco de dados e adicionando variaveis de ambiente no .env

--- scraper.py
+++ scraper.py
@@ -2,36 +2,36 @@ import smtplib
 import csv
 import os
 import cloudscraper
+import mysql.connector
 
 from bs4 import BeautifulSoup
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from datetime import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def checkPrices():
-    # Caminho para o arquivo CSV
-    csvFile = 'mail_history.csv'
+    conexao = mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DATABASE"),
+    )
+
+    cursor = conexao.cursor(buffered=True)
 
     todayDate = datetime.today().strftime('%Y-%m-%d')
-
-    currentHour = datetime.today().strftime('%H:%M')
-    resetCsv = False
-
-    if(currentHour == '00:00'):
-        resetCsv = True
-        
-        with open(csvFile, 'w', encoding='utf-8') as arquivo:
-            # O conteúdo do arquivo é truncado, deixando-o vazio
-            pass
 
     #   Itens que vão ser monitorados
     itens = {}
         
     # Pega id e preço de cada item do env
     count = 1
-    while os.environ.get('item_id_'+str(count)) is not None:
-        item_id = os.environ.get('item_id_'+str(count))
-        price_id = os.environ.get('price_id_'+str(count))
+    while os.getenv('ITEM_ID_'+str(count)) is not None:
+        item_id = os.getenv('ITEM_ID_'+str(count))
+        price_id = os.getenv('PRICE_ID_'+str(count))
 
         itens[item_id] = price_id
 
@@ -75,38 +75,35 @@ def checkPrices():
             if("CASH" in rows.find_all('td')[5].text):
 
                 if(int(rows.find('font').text.replace(',', '')) <= int(itemPrice)):
-                    storeName = rows.find_all('td')[0].text
-                    refinement = rows.find_all('td')[1].text
-                    cards = rows.find_all('td')[2].text
-                    price = rows.find_all('td')[3].text
-                    quantity = rows.find_all('td')[4].text
+                    storeName = (rows.find_all('td')[0].text).strip()
+                    refinement = (rows.find_all('td')[1].text).strip()
+                    cards = (rows.find_all('td')[2].text).strip()
+                    price = (rows.find_all('td')[3].text).replace("c", "").strip()
+                    quantity = (rows.find_all('td')[4].text).strip()
 
                     #   Adicionando valores em cada uma das células da tabela
                     if(storeName):
                         formattedPrice = int(rows.find('font').text.replace(',', ''))
 
-                        # Linha que você deseja procurar
-                        specificLine = [todayDate, str(itemId), str(formattedPrice)]
-
                         # Variável para indicar se a linha foi encontrada
                         foundLine = False
 
                         # Procura se já foi enviado um email hoje com o alerta do item
-                        with open(csvFile, newline='', encoding='utf-8') as csvfile:
-                            leitor = csv.reader(csvfile)
-                            for linha in leitor:
-                                # Comparando a linha atual com a linha específica
-                                if linha == specificLine:
-                                    foundLine = True
-                                    break
+                        formattedPrice = "{:.2f}".format(float(price))
+                        comando = f"SELECT * FROM alerts WHERE item_id = '{itemId}' AND store_name = '{storeName}' AND price = '{formattedPrice}' AND date = '{todayDate}';"
+                        cursor.execute(comando)
+
+                        if(cursor.rowcount > 0):
+                            foundLine = True
+                        
 
                         if(foundLine == False):
                             sendMessage = True
 
-                            # Adiciona no arquivo csv e no html o item
-                            with open('mail_history.csv', 'a') as f:
-                                writer = csv.writer(f)
-                                writer.writerow([todayDate, itemId, formattedPrice])
+                            # Adiciona no banco o item
+                            comando = f"INSERT INTO alerts (name, item_id, refinement, store_name, price, date) VALUES ('{itemName}', '{itemId}', '{refinement}', '{storeName}', '{formattedPrice}', '{todayDate}')"
+                            cursor.execute(comando)
+                            conexao.commit()
 
                             rowItem += """   <tr>
                                                 <td>""" + storeName + """</td>
@@ -155,12 +152,12 @@ def checkPrices():
         </html>
     """
 
-    if(sendMessage == True and resetCsv == False):
+    if(sendMessage == True):
         subject = "History Reborn - Alerta atingido"
         body = html
-        senderEmail = os.environ.get("SENDER_EMAIL")
-        recipientEmail = os.environ.get("RECIPIENT_EMAIL")
-        senderPassword = os.environ.get("GMAIL_TOKEN")
+        senderEmail = os.getenv("SENDER_EMAIL")
+        recipientEmail = os.getenv("RECIPIENT_EMAIL")
+        senderPassword = os.getenv("GMAIL_TOKEN")
         smtpServer = 'smtp.gmail.com'
         smtpPort = 465
 
@@ -175,6 +172,8 @@ def checkPrices():
             server.login(senderEmail, senderPassword)
             server.sendmail(senderEmail, recipientEmail, message.as_string())
 
+    cursor.close()
+    conexao.close()
     return "Executado com sucesso!"
 
 # if __name__ == "__main__":
