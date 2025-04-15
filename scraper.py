59d4d3760aa68b57cdb702941ec29e1bb1be011e
Renisson Silva <renissonx@gmail.com>
2025-04-15 10:15:47 -0300

    Tirando comentário da checkprices

--- scraper.py
+++ scraper.py
@@ -294,7 +294,7 @@ if __name__ == "__main__":
     for i in range(4):
         current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         print(f"Execução {i + 1} de 5 - Horário: {current_time}")
-        # checkPrices()
+        checkPrices()
         current_time_final = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         print(f"Terminou {i + 1} de 5 - Horário: {current_time_final}")
 
